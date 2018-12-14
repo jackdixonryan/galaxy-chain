6eeca65126f34252f3043c9142dca301eaaf1fca
Jack Ryan <jack.dixon.ryan@gmail.com>
2018-12-14 13:06:35 -0700

    There are going to be issues with screen resizing but otherwise, the ether info page is done! Cool scrolling parallax images, awesome triggered animations, a fully interactive and awesome experience. I'm proud.

--- frontend/src/pages/Ether.vue
+++ frontend/src/pages/Ether.vue
@@ -23,18 +23,18 @@
         <v-parallax src="https://i.imgur.com/gQp3VSW.jpg" id="parallax-img" height="400"></v-parallax>
         <h1 
           class="display-1 section-title" 
-          :class="{ 'animated fadeIn white--text' : (scroll >= 771) }"
+          :class="{ 'animated fadeIn white--text' : (scroll >= 650) }"
         >What is Ether?</h1>
         <p 
           class="justified" 
-          :class="{ 'animated fadeIn white--text delay-1s' : (scroll >= 771) }"
+          :class="{ 'animated fadeIn white--text delay-1s' : (scroll >= 650) }"
         >
           Ether is a distributed ledger system that creates secure, distributed, immutable smart contracts. For you, this means that all of your transactions are safe and verified by a wide network of machines.</p>
         <v-timeline dense>
           <v-timeline-item color="green">
             <span slot="opposite"></span>
             <v-card class="elevation-2 hidden" 
-              :class="{'animated rotateIn visible' : scroll >= 1280 }">
+              :class="{'animated fadeIn visible' : scroll >= 1100 }">
               <v-card-title class="headline centered">Pay</v-card-title>
               <v-card-text class="justified">
                 When you place a transaction on the ethereum network, your balance will be evaluated to see if you have sufficient ether to complete the request.
@@ -43,7 +43,7 @@
           </v-timeline-item>
           <v-timeline-item color="green">
             <span class="opposite"></span>
-            <v-card class="elevation-2 hidden" :class="{'animated rotateIn visible delay-1s' : scroll >= 1280 }">
+            <v-card class="elevation-2 hidden" :class="{'animated fadeIn visible' : scroll >= 1200 }">
               <v-card-title class="headline centered">Pend</v-card-title>
               <v-card-text class="justified">
                 You will notice that all of your transactions take a moment to register. The Ethereum network requires validation when writing data.
@@ -52,7 +52,7 @@
           </v-timeline-item>
           <v-timeline-item color="green">
             <span class="opposite"></span>
-            <v-card class="elevation-2 hidden" :class="{'animated rotateIn visible delay-2s' : scroll >= 1280 }">
+            <v-card class="elevation-2 hidden" :class="{'animated fadeIn visible' : scroll >= 1300 }">
               <v-card-title class="headline centered">Post</v-card-title>
               <v-card-text class="justified">
                 You will shortly receive a message telling you that your transaction was successfully posted. The reason for the delay is that your transaction has to be added to the ethereum blockchain, a process called mining. 
@@ -67,21 +67,21 @@
     <v-layout row wrap>
       <v-flex xs8 offset-xs2 id="what-for">
         <v-parallax src="https://www.universetoday.com/wp-content/uploads/2018/08/cropped-delta_iv_heavy_streak_composite_edits.jpg" id="parallax-img" height="400"></v-parallax>
-        <p class="display-1 white--text section-title">What am I paying for?</p>
-        <p class="justified white--text">You will notice while experimenting with galaxy that there are transaction amounts for everything, even when you aren't trying to spend money. When working with Ethereum smart contracts, every transfer of data costs a bit of money, something ethereum programmers call 'gas.'</p>
-        <p class="justified white--text">Gas is a small amount of ether attached to every transaction you make with the ethereum blockchain. You can think of it as a sort of sales tax. If you try to buy/sell an item, it will have a gas fee attached to it.</p>
-        <p class="justified white--text">Where you will find yourself confused is when you are trying to transact without spending any money. Some of the features on Galaxy don't expressly require spending money: Voting, registering in a nation-state, and making proposals all require a small transaction fee for making the transaction.</p>
+        <p class="display-1 section-title" :class="{ 'animated fadeIn white--text' : (scroll >= 2100) }">What am I paying for?</p>
+        <p class="justified" :class="{ 'animated fadeIn white--text delay-1s' : (scroll >= 2100) }">You will notice while experimenting with galaxy that there are transaction amounts for everything, even when you aren't trying to spend money. When working with Ethereum smart contracts, every transfer of data costs a bit of money, something ethereum programmers call 'gas.'</p>
+        <p class="justified" :class="{ 'animated fadeIn white--text delay-2s' : (scroll >= 2100) }">Gas is a small amount of ether attached to every transaction you make with the ethereum blockchain. You can think of it as a sort of sales tax. If you try to buy/sell an item, it will have a gas fee attached to it.</p>
+        <p class="justified" :class="{ 'animated fadeIn white--text delay-3s' : (scroll >= 2100) }">Where you will find yourself confused is when you are trying to transact without spending any money. Some of the features on Galaxy don't expressly require spending money: Voting, registering in a nation-state, and making proposals all require a small transaction fee for making the transaction.</p>
       </v-flex>
     </v-layout>
     <v-layout row wrap>
       <v-flex xs8 offset-xs2 id="get-ether">
         <v-parallax src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/EmissionNebula_NGC6357.jpg/440px-EmissionNebula_NGC6357.jpg" id="parallax-img" height="400"></v-parallax>
-        <p class="display-1 white--text section-title">How to get Ether</p>
-        <p class="justified white--text">Because Galaxy chain is primarily a learning app so people can have fun learning how to interact with the ethereum network, it is designed only to run on a testing platform called the Rinkeby test network. This means that if you choose to play, it's completely free!</p>
-        <p class="justified white--text">It's easy to get ether via the test network. Just follow these steps.</p>
+        <p class="display-1 section-title" :class="{ 'animated fadeIn white--text' : (scroll >= 2900) }">How to get Ether</p>
+        <p class="justified" :class="{ 'animated fadeIn white--text delay-1s' : (scroll >= 2900) }">Because Galaxy chain is primarily a learning app so people can have fun learning how to interact with the ethereum network, it is designed only to run on a testing platform called the Rinkeby test network. This means that if you choose to play, it's completely free!</p>
+        <p class="justified white--text" :class="{ 'animated fadeIn white--text delay-2s' : (scroll >= 2900) }">It's easy to get ether via the test network. Just follow these steps.</p>
         <v-timeline dense>
           <v-timeline-item color="green">
-            <v-card class="elevation-2">
+            <v-card class="elevation-2 hidden" :class="{ 'animated fadeIn visible' : (scroll >= 3200) }">
               <v-card-title class="headline">
                 <a href="https://faucet.rinkeby.io/" target="_blank" class="rky-link">Go to the Rinkeby Ether Faucet</a>
               </v-card-title>
@@ -91,7 +91,7 @@
             </v-card>
           </v-timeline-item>
           <v-timeline-item color="green">
-            <v-card class="elevation-2">
+            <v-card class="elevation-2 hidden" :class="{ 'animated fadeIn visible' : (scroll >= 3400) }">
               <v-card-title class="headline">
                 Choose a Social Media Site
               </v-card-title>
@@ -101,7 +101,7 @@
             </v-card>
           </v-timeline-item>
           <v-timeline-item color="green">
-            <v-card class="elevation-2">
+            <v-card class="elevation-2 hidden" :class="{ 'animated fadeIn visible' : (scroll >= 3600) }">
               <v-card-title class="headline">Copy/Paste Your Address</v-card-title>
               <v-card-text class="justified">
                 Once you're ready to post/tweet, you will need your ethereum public address pasted in the text. In the below example, this address can be copied by clicking the text that says 'Account 1' at the top of the window.
@@ -111,7 +111,7 @@
             </v-card>
           </v-timeline-item>
           <v-timeline-item color="green">
-            <v-card class="elevation-2">
+            <v-card class="elevation-2 hidden" :class="{ 'animated fadeIn visible' : (scroll >= 4050) }">
               <v-card-title class="headline">Wait</v-card-title>
               <v-card-text class="justified">
                 As stated above, everything ethereum takes a bit of time. It will take a bit of time for ethereum wallet to show your balance as being updated. As soon as it is, however, you're ready to jump in and start playing! 
