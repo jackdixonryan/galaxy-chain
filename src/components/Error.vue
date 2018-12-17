1009361d112c1c55da29901e4958b04c4c0f3999
Jack Ryan <jack.dixon.ryan@gmail.com>
2018-12-17 08:57:31 -0700

    Something went wrong with the last commit, didn't deploy, so we're going to see what happened.

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -1,6 +1,7 @@
 <template>
   <section id="error-main">
-    <h1 class="display-1 blue--text">uhoh</h1>
+    <h1 class="display-1 blue--text">Something Went Wrong</h1>
+    <p>We detected an issue with your connection to web3. This could be for a variety of reasons:</p>
   </section>
 </template>
 
