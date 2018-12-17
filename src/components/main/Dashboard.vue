e9219b550700076f7a8b42a2408971dbfb841918
Jack Ryan <jack.dixon.ryan@gmail.com>
2018-12-17 13:11:17 -0700

    Working out the feed and the tutorial.

--- src/components/main/Dashboard.vue
+++ src/components/main/Dashboard.vue
@@ -13,47 +13,16 @@
         <v-card-text>
           This page is your main console, where you can view basic information about your data in the galaxy. As it's your first time, there's a couple things we're going to need to set up. 
           <div v-if="balance === '0'">
-            <v-divider></v-divider>
-            The first step of any successful galactic journey is to check one's wallet. Let's do that now.
+            <br>
+            The first step of any successful galactic journey is to check one's wallet. We noticed you haven't got any currency! Click on your balance if you need some information on starting out with Ethereum.
           </div>
           <div v-else>
-            <v-divider></v-divider>
             Looks like you've got a little ether already, stranger. You might just make it out here. 
           </div>
         </v-card-text>
         <v-card-actions>
-          <v-btn @click="dialog=false" flat class="indigo">Continue</v-btn>
+          <v-btn @click="dialog=false" flat class="white">Continue</v-btn>
         </v-card-actions>
-      </v-card>
-    </v-dialog>
-
-    <v-dialog
-      v-model="getEtherDialog"
-      fullscreen
-    >
-      <v-card>
-        <v-card-title class="headline">
-          Ether Guide
-        </v-card-title>
-        <v-card-text>
-          <v-container text-xs-center>
-            <v-layout row wrap>
-              <v-flex xs12>
-                <div>
-                  <v-btn>What is Ether?</v-btn>
-                  <v-btn>The Basics of Transactions</v-btn>
-                  <v-btn>Getting Ether</v-btn>
-                </div>
-              </v-flex>
-            </v-layout>
-            <v-layout row wrap>
-              <v-flex xs6>Getting Ether on the Rinkeby test network is going to be the most difficult step to overcome.</v-flex>
-              <v-flex xs6></v-flex>
-              <v-flex xs6></v-flex>
-              <v-flex xs6></v-flex>
-            </v-layout>
-          </v-container>
-        </v-card-text>
       </v-card>
     </v-dialog>
 
@@ -76,17 +45,36 @@
         </v-card>
       </v-flex>
     </v-layout>
-    <v-layout row wrap>
-      <v-flex xs12 sm6>
+    <v-layout row wrap id="dashboard-main">
+      <v-flex xs12 sm4>
         <v-card>
-          <v-card-text>
-          
-          </v-card-text>
+          <v-card-title>
+            <p v-if="citizen">Citizen Of {{citizen.country}} since {{citizen.year}}</p>
+            <p v-else>You aren't in a country yet! <router-link to="/register/country">You can sign up for one here.</router-link></p>
+            <p>Once you sign up for a country affiliation, you will see a lot of information on this page. Selecting a nation opens the door to markets, trades, voting, and a lot more. Plus, you'll get access to your nation's forum page where you can communicate with your fellow citizens.</p>
+          </v-card-title>
         </v-card>
       </v-flex>
-      <v-flex xs12 sm6>
-        <v-card>
-          <v-card-text>News</v-card-text>
+      <v-flex xs12 sm8 id="news-column">
+        <div class="headline blue--text">News Feed</div>
+        <v-card class="feed-item">
+          <v-card-title>
+            <div class="title">Citizens of Mercalla reelect their council</div>
+            <div class="subtitle" style="width:100%;">12/17/18</div>
+            <v-divider style="width:100%;"></v-divider>
+            <div class="main">Here You will see major information regarding news stories from the people in your feed. </div>
+          </v-card-title>
+        </v-card>
+        <v-card class="feed-item">
+          <v-card-title>
+            <div class="title">Listing Items for Sale</div>
+            <div class="subtitle">12/15/18</div>
+            <div class="main">I'm looking to sell 20 calcium pills at .0005 eth a piece, tradable with Tajiri merchants for ship pieces.</div>
+          </v-card-title>
+          <v-card-actions>
+            <div>18 Remaining</div>
+            <v-btn flat color="indigo">Buy</v-btn>
+          </v-card-actions>
         </v-card>
       </v-flex>
     </v-layout>
@@ -124,11 +112,28 @@ export default {
 </script>
 
 <style  scoped>
+
   #eth-balance {
     float: right;
     cursor: pointer;
   }
+
+  #dashboard-main {
+    margin-top: 1em;
+  }
+
   #balance {
     padding: 5px;
   }
+
+  #news-column {
+    text-align: center;
+    padding: 1em;
+  }
+
+  .feed-item {
+    margin-top: 1em;
+    text-align: left;
+  }
+
 </style>
